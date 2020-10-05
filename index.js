a95df7977f77cfd76ebd75f974d9133aaec476c4
Brayden England <bray.patriot@gmail.com>
2020-10-04 23:28:32 -0600

    Added process.env.PORT for Heroku

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 // built in express body parser middleware
 app.use(express.json())
